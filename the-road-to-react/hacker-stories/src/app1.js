2fc632c15d48ab1c8e718abc50d33bae66fb1cf3
Daniel Hemmati <danialups1997@gmail.com>
2021-04-17 17:22:39 +0200

    now i understand, i am so happy

--- the-road-to-react/hacker-stories/src/app1.js
+++ the-road-to-react/hacker-stories/src/app1.js
@@ -1,34 +1,73 @@
 import React, { useState } from "react";
 
-const initialList = [
-  "daniel full-stack/data science/ML/hacker",
-  "daniel back-end",
-];
-function Test() {
-  const [list, setList] = useState(initialList);
+function Hello() {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-  function handleClick(event) {
-    // i don't know why we are using slice in here
-    setList(list.slice().reverse());
-  }
+  // one state which is going to be used in both Search and List component
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const SearchStories = stories.filter((item) => {
+    // searchTerm witll be update via setSearchTerm, so we want to filter the value we have right now
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
 
   return (
     <div>
-      <h1>hello there</h1>
-
-      {list.map((item, index) => {
-        return (
-          <li key={index}>
-            <input type="checkbox" />
-            <label>{item}</label>
-          </li>
-        );
-      })}
-      <button type="submit" onClick={handleClick}>
-        reverse me{" "}
-      </button>
+      <h1>hello world</h1>
+      {/* showType is just for me */}
+      <Search onSearch={handleChange} showType={searchTerm} />
+      <List list={SearchStories} />
     </div>
   );
 }
 
-export default Test;
+const Search = (props) => {
+  return (
+    <>
+      <h3>{props.showType}</h3>
+      <label htmlFor="serach">Search: </label>
+      <input type="text" id="search" onChange={props.onSearch} />
+      <br />
+      <br />
+    </>
+  );
+};
+
+const List = ({ list }) => {
+  // here we are mapping through the filter item
+  return list.map((item) => {
+    return (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.point}</span>
+        <span>{item.num_comments}</span>
+      </div>
+    );
+  });
+};
+
+export default Hello;
