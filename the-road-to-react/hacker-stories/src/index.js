cea3c5d72fe23727d79b6d7b9e5a6d7e70c94da7
Daniel Hemmati <danialups1997@gmail.com>
2021-04-20 00:16:44 +0200

    see ya tomorrow

--- the-road-to-react/hacker-stories/src/index.js
+++ the-road-to-react/hacker-stories/src/index.js
@@ -5,7 +5,8 @@ import reportWebVitals from "./reportWebVitals";
 // import App from "./App";
 // import App from "./react-hooks";
 // import App from "./overReacted";
-import App from "./useReducer";
+// import App from "./useReducer";
+import App from "./fetchDataUseEffect";
 // import App from "./stopRender";
 
 ReactDOM.render(<App />, document.getElementById("root"));
