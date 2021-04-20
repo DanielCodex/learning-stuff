93d2727ae6b2e79826beb4e381af3693f40ce2d0
Daniel Hemmati <danialups1997@gmail.com>
2021-04-20 16:41:12 +0200

    now i want to rewrite this counter in reducer

--- the-road-to-react/hacker-stories/src/useReducer.js
+++ the-road-to-react/hacker-stories/src/useReducer.js
@@ -1,36 +1,23 @@
-import React, { useEffect, useRef, useState } from "react";
-
-const useEffectOnUpdate = (callback, dependencies) => {
-  const didMount = useRef(false);
-  useEffect(() => {
-    if (didMount.current) {
-      callback(dependencies);
-    } else {
-      didMount.current = true;
-    }
-  }, [callback, dependencies]);
-};
+import React, { useState } from "react";
 
 function App() {
-  const [toggle, setToggle] = useState(true);
-
-  const handleChange = () => {
-    setToggle(!toggle);
+  const [count, setCount] = useState(0);
+  const handleIncrease = () => {
+    setCount(count + 1);
   };
 
-  useEffectOnUpdate((dependencies) => {
-    console.log(dependencies);
-    console.log("i run only if toggle changes");
-  });
-
+  const handleDecrease = () => {
+    setCount(count - 1);
+  };
   return (
     <div>
-      <button type="button" onClick={handleChange}>
-        toggle
+      <h1>{count}</h1>
+      <button type="button" onClick={handleIncrease}>
+        increase
       </button>
-      <br />
-      <br />
-      {toggle && <div>hello react</div>}
+      <button type="button" onClick={handleDecrease}>
+        decrease
+      </button>
     </div>
   );
 }
