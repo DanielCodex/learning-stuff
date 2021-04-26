a3d162ff10ac1387d98929b0cf2e254ecb0601ef
Daniel Hemmati <danialups1997@gmail.com>
2021-04-26 13:47:56 +0200

    add clear button, and handle undefined item
    if you just write *daniel* it will add undefined
    for lastname, and year.

--- the-road-to-react/hacker-stories/src/removeItem.js
+++ the-road-to-react/hacker-stories/src/removeItem.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuid4 } from "uuid";
 
-const list = [
+const initialList = [
   {
     id: "a",
     firstname: "danile",
@@ -13,16 +14,56 @@ const list = [
     lastname: "hemmati",
     year: 1990,
   },
+  {
+    id: "c",
+    firstname: "mina",
+    lastname: "sabzian",
+    year: 2000,
+  },
 ];
 
 function App() {
+  const [list, setList] = useState(initialList);
+  const [name, setName] = useState("");
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleClick = (index) => {
+    const removeItem = list.filter((item) => item.id !== index);
+    setList(removeItem);
+  };
+
+  const addItem = (foo) => {
+    // const newItem = list.concat({ firstname: name });
+    // console.log(name);
+    let splitName = name.split(" ");
+    setList(
+      list.concat({
+        id: uuid4(),
+        firstname: splitName[0],
+        lastname: splitName[1],
+        year: splitName[2],
+      })
+    );
+  };
+
   return (
     <ul>
+      {/* <h1>{name}</h1> */}
+      <input type="text" value={name} onChange={handleChange} />
+      <button type="button" onClick={addItem}>
+        add new item
+      </button>
       {list.map((item) => (
         <li key={item.id}>
           {" "}
           <span>{item.firstname}</span> <span>{item.lastname}</span>{" "}
           <span>{item.year}</span>
+          <button type="button" onClick={() => handleClick(item.id)}>
+            remove Item
+          </button>
         </li>
       ))}
     </ul>
