a47ada60d85aafafab7d2399bdd1c8d60296d76f
Daniel Hemmati <danialups1997@gmail.com>
2021-04-29 14:03:52 +0200

    add button handler

--- the-road-to-react/hacker-stories/src/classComponent.js
+++ the-road-to-react/hacker-stories/src/classComponent.js
@@ -1,27 +1,37 @@
 import React, { Component } from "react";
 
+// easy
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTerm: "react",
+      searchTerm: "daniel hemmati",
+      text: "",
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange(e) {
     this.setState({ searchTerm: e.target.value });
   }
 
+  handleClick() {
+    this.setState({ text: this.state.searchTerm });
+  }
+
   render() {
     return (
       <div>
-        <h1>{this.state.searchTerm}</h1>
+        <h1>{this.state.text}</h1>
         <input
           type="text"
           value={this.state.searchTerm}
           onChange={this.handleChange}
         />
+        <button type="button" onClick={this.handleClick}>
+          now show it
+        </button>
       </div>
     );
   }
