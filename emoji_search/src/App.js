a2b5b8981a898d24d6bd30f925f2954b1702e1ca
Daniel Hemmati <danialups199@gmail.com>
2021-05-16 21:58:43 +0200

    design part is done

--- emoji_search/src/App.js
+++ emoji_search/src/App.js
@@ -1,11 +1,50 @@
-import "./App.css";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styled from "styled-components";
+import emoji from "./emoji-list";
 
 function App() {
+  const [list, setList] = useState(emoji);
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <div className="container">
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <h1>hello world</h1>
+        <EmojiList list={list} />
+      </div>
     </div>
   );
 }
+
+const ListStyleUL = styled.ul`
+  list-style: none;
+  font-size: 20px;
+  margin: 0;
+  padding: 0;
+  width: 500px;
+`;
+
+const ListStyle = styled.li`
+  text-align: left;
+  border: 1px solid white;
+  padding: 9px;
+`;
+
+const EmojiList = ({ list }) => {
+  return (
+    <div>
+      <ListStyleUL>
+        {list.map((item) => {
+          return (
+            <>
+              <ListStyle>
+                {item.symbol} {item.title} <br />
+              </ListStyle>
+            </>
+          );
+        })}
+      </ListStyleUL>
+    </div>
+  );
+};
 
 export default App;
--- emoji_search/src/index.js
+++ emoji_search/src/index.js
@@ -2,8 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { createGlobalStyle } from "styled-components";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #222;
+    color: white;
+  }
+`;
+
+ReactDOM.render(
+  <>
+    <App />
+    <GlobalStyle />
+  </>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
