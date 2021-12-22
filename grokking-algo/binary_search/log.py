6d114366ef1f98012f651f3a0b4da3f791328e77
danielhemmati <danialups1997@gmail.com>
2021-12-22 19:49:39 +0100

    it's been a long time
    i couldn't see because of my eyes for two month
    and i was learning how to run youtube. now the first and second
    is solve. i want to run youtube channel, and i know how í¸

---
+++ grokking-algo/binary_search/log.py
@@ -0,0 +1,29 @@
+import math
+
+print(math.log2(4000000000))
+
+
+def log(x, base):
+    result = ln(x)/ln(base)
+    return result
+
+
+def ln(x):
+    n = 100000.0
+    return n * ((x ** (1/n)) - 1)
+
+
+print([i for i in range(0, 10)])
+
+
+# print(log(4, 2))
+# if you have arrays of number
+# a = [10.000, 20.000, 25.000]
+# b = [int(i) for i in a]
+# print(b)
+
+
+# if you have arrays of number which are string
+# a = ["10.000", "20.000", "25.000"]
+# b = [i.replace(".", "") for i in a]
+# print(b)
